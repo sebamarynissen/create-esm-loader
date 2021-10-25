f9894ed72bd76e7adb8f3da33d904007e60a7da6
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2021-10-25 17:30:32 +0200

    Fix running tests from root directory

--- test/run.js
+++ test/run.js
@@ -1,15 +1,18 @@
 // # run.js
 import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 // Note: mocha will try to run this file, but it obviously shouldn't. Hence, if 
 // we're not running as a forked process, we do nothing.
 if (process.send) {
 	const [,, base64] = process.argv;
 	const code = Buffer.from(base64, 'base64');
-	const file = './code.js';
+	const url = new URL('./code.js', import.meta.url);
+	const file = fileURLToPath(url);
 	try {
 		await fs.writeFile(file, code);
-		const module = await import(file);
+		const module = await import(url);
 		process.send(module.default);
 	} finally {
 		fs.unlink(file);
